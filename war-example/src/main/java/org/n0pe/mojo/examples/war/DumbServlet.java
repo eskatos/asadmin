8f79e65603076ebf3728bde84ced8a62679cde9c
Paul Merlin <paul@nosphere.org>
2008-08-03 15:01:01 +0000

    war-example : added a servlet to the webapp

---
+++ war-example/src/main/java/org/n0pe/mojo/examples/war/DumbServlet.java
@@ -0,0 +1,50 @@
+package org.n0pe.mojo.examples.war;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+
+public class DumbServlet
+        extends HttpServlet {
+
+
+    private static final long serialVersionUID = 1L;
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet DumbServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet DumbServlet at " + request.getContextPath() + "</h1>");
+            out.println("<p>RandomString: " + RandomStringUtils.random(16) + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendError(response.SC_FORBIDDEN);
+    }
+
+
+}
