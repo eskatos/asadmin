9d963ed25dc6ba9dac09f3e8cff12a17dd8d7b60
Paul Merlin <paul@nosphere.org>
2008-10-18 15:54:13 +0000

    splitted the projet so the java code wrapping the asadmin command is usable alone

---
+++ src/main/java/org/n0pe/asadmin/AsAdminCmdList.java
@@ -0,0 +1,86 @@
+/**
+ * asadmin-glassfish-plugin : a maven plugin for glassfish administratives tasks
+ * 
+ * Copyright (C) 2008  Paul Merlin
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.n0pe.asadmin;
+
+
+import java.util.ArrayList;
+
+
+/**
+ * An ArrayList containing IAsCommands.
+ * Generics in maven ?
+ *
+ * @author Paul Merlin <eskatos@n0pe.org>
+ */
+public class AsAdminCmdList
+        extends ArrayList {
+
+
+    private static final long serialVersionUID = 1L;
+
+
+    /**
+     * AsAdminCmdList CTOR.
+     */
+    public AsAdminCmdList() {
+        super();
+    }
+
+
+    public boolean add(IAsAdminCmd e) {
+        return super.add(e);
+    }
+
+
+    public void add(int index, IAsAdminCmd element) {
+        super.add(index, element);
+    }
+
+
+    public boolean contains(IAsAdminCmd o) {
+        return super.contains(o);
+    }
+
+
+    public int indexOf(IAsAdminCmd o) {
+        return super.indexOf(o);
+    }
+
+
+    public int lastIndexOf(IAsAdminCmd o) {
+        return super.lastIndexOf(o);
+    }
+
+
+    public boolean remove(IAsAdminCmd o) {
+        return super.remove(o);
+    }
+
+
+    public IAsAdminCmd set(int index, IAsAdminCmd element) {
+        return (IAsAdminCmd) super.set(index, element);
+    }
+
+
+    public IAsAdminCmd[] toArray(IAsAdminCmd[] a) {
+        return (IAsAdminCmd[]) super.toArray(a);
+    }
+
+
+}
